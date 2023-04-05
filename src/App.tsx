3d9f57c8f3c530fd8ff4d21ee74bc4ce21ce2046
Rares Herta <rares.herta@endava.com>
2023-04-05 17:02:31 +0300

    Add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { Home } from './components/Home/Home';
 import { Store } from './components/Store/Store';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
 	return (
-		<Container className='mb-4'>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/store' element={<Store />} />
-			</Routes>
-		</Container>
+		<>
+			<Navbar />
+			<Container className='mb-4'>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/store' element={<Store />} />
+				</Routes>
+			</Container>
+		</>
 	);
 }
 
