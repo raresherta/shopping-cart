5cfcc858cdc70cd87894ebc94536ad1e4c68fdef
Rares Herta <rares.herta@endava.com>
2023-04-07 11:28:31 +0300

    Add shipping cost

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { Offcanvas, Stack } from 'react-bootstrap';
 import { useShoppingCart } from '../../context/ShoppingCartContext';
 import { CartItem } from '../CartItem/CartItem';
@@ -8,7 +10,37 @@ type ShoppingCartProps = {
 };
 
 export function ShoppingCart({ isOpen }: ShoppingCartProps) {
-	const { closeCart, cartItems, products } = useShoppingCart();
+	const { closeCart, cartItems, products, cartQuantity } = useShoppingCart();
+	var shippingCost = 0;
+
+	useEffect(() => {
+		addShippingCost(cartQuantity);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [cartQuantity, shippingCost]);
+
+	function addShippingCost(num: number) {
+		if (num < 20) {
+			shippingCost = 7;
+			return formatPrice(7);
+		} else if (num < 40) {
+			shippingCost = 5;
+			return formatPrice(5);
+		} else {
+			return 'FREE';
+		}
+	}
+
+	function calculateTotalPrice() {
+		const totalItemsPrice = cartItems.reduce((total, cartItem) => {
+			const item = products.find((i: any) => i.id === cartItem.id);
+			return total + (item?.price || 0) * cartItem.quantity;
+		}, 0);
+
+		const totalPrice = totalItemsPrice + shippingCost;
+
+		return formatPrice(totalPrice);
+	}
+
 	return (
 		<Offcanvas show={isOpen} onHide={closeCart} placement='end'>
 			<Offcanvas.Header closeButton>
@@ -19,19 +51,14 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
 					{cartItems.map(item => (
 						<CartItem key={item.id} {...item} />
 					))}
+					<div className='d-flex justify-content-end fs-7'>
+						<span style={{ paddingRight: '8px' }}>
+							Shipping Cost{''}
+						</span>
+						<span>{addShippingCost(cartQuantity)}</span>
+					</div>
 					<div className='ms-auto fw-bold fs-5'>
-						Total{' '}
-						{formatPrice(
-							cartItems.reduce((total, cartItem) => {
-								const item = products.find(
-									(i: any) => i.id === cartItem.id
-								);
-								return (
-									total +
-									(item?.price || 0) * cartItem.quantity
-								);
-							}, 0)
-						)}
+						Total {calculateTotalPrice()}
 					</div>
 				</Stack>
 			</Offcanvas.Body>
